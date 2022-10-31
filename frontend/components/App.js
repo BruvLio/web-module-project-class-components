b9d630f0397691e4b055bf047e4024783dc31118
Lionel Mbayu <Lionel.Mbayu@gmail.com>
2022-10-31 14:56:11 -0500

    Buils from state and completed flags

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,18 +1,41 @@
 import React from 'react'
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: [
+        {
+          name: 'Organize Garage',
+          id: 1528817077286, 
+          completed: false
+        },
+        {
+          name: 'Bake Cookies',
+          id: 1528817084358,
+          completed: false
+        },
+      ]
+    }
 
+  }
 
 
 
   render() {
+    const { todos } = this.state;
+    console.log(todos);
     return (
       <div>
         <h1>Todos</h1>
         <ul>
-          <li>Walk the dog</li>
-          <li>Take out Trash</li>
-          <li>Workout</li>
+          {
+            todos.map( todo => {
+              return (<li key={todo.id}> {todo.name} { todo.completed?<span>- compoleted</span> : <span></span>}</li>)
+            })
+          }
+
+
         </ul>
 
         <form>
